cbf9b3a400239cc83b0e974897d02bd2a08fb745
SaborLatte <sanchez.villegas.fernana.c@gmail.com>
2025-03-10 17:20:44 -0600

    first commit

---
+++ Estadisticadescriptiva/01Rango.py
@@ -0,0 +1,14 @@
+#vamos a crear un programa que pregunte al usuario por las ventas de un rango de años que muestre
+
+import pandas as pd
+inicio= int(input('introduce el año inicial enventas:'))
+fin= int(input('Introduce el año final de ventas:'))
+
+ventas={}
+
+for i in range (inicio, fin+1):
+    ventas[i]= float(input('Introduce las ventas del año' +str(i) + ':'))
+    
+ventas= pd.Series(ventas)
+print('Ventas \n', ventas)
+print('Ventas con Descuento \n', ventas*0.9)
