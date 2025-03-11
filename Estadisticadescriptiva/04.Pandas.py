62e90a90251f1aa6315cf702d991fe4ec335a264
SaborLatte <sanchez.villegas.fernana.c@gmail.com>
2025-03-10 18:15:12 -0600

    first commit

--- Estadisticadescriptiva/04.Pandas.py
+++ Estadisticadescriptiva/04.Pandas.py
@@ -3,13 +3,19 @@ import pandas as pd
 df=pd.read_csv('./Estadisticadescriptiva/housing.csv')
 
 print(df.head())
+
 print(df.tail())
+
 print(df.iloc[7])
+
 print(df["ocean_proximity"])
+
 mediacuartos= df["total_rooms"].mean()
 print('Media de cuartos', mediacuartos)
+
 medianapopularidad= df["population"].median()
 print('Mediana de plpularidad: ', medianapopularidad)
+
 std_age=df["housing_median_age"].std()
 print('Desviacion estandar de los años: ', std_age)
 
